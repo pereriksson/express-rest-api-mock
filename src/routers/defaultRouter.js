76ac1cd09b8888b41a91701aaa6843ea456c4586
Per Eriksson <per@interapp.se>
2021-01-04 03:39:16 +0100

    added a get route for a specific resource

--- src/routers/defaultRouter.js
+++ src/routers/defaultRouter.js
@@ -31,6 +31,19 @@ router.delete('/:collection/:id', (req, res) => {
     }
 });
 
+router.get('/:collection/:id', (req, res) => {
+    let collection = req.params.collection;
+    let id = parseInt(req.params.id);
+
+    let entity = data[collection].find((e) => e.id === id);
+
+    if (entity) {
+        res.status(200).send(JSON.stringify(entity));
+    } else {
+        res.status(404).send();
+    }
+});
+
 router.put('/:collection/:id', (req, res) => {
     let collection = req.params.collection;
     let id = parseInt(req.params.id);
